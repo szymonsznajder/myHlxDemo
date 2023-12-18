91238e95ddd8a833f0a4b77a8ddf7fd2e94f61a5
Szymon Sznajder <sznajder.szymon@gmail.com>
2023-12-18 16:30:22 +0000

    Blockquote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,13 +1,13 @@
 export default function decorate(block){
     const quoteDiv = block.querySelector(':scope > div > div');
-const blockquote = document.createElement(tagName: 'blockquote');
-blockquote.innerHTML = `"${quoteDiv,innerHTML}"`;
-quoteDiv.parentElement.replaceWidth(blockquote);
+    const blockquote = document.createElement(tagName: 'blockquote');
+    blockquote.innerHTML = `"${quoteDiv.innerHTML}"`;
+    quoteDiv.parentElement.replaceWidth(blockquote);
 
-const authorDiv = block.querySelector(':scope > div > div');
-if(authorDiv){
-const p = document.createElement(tagName: 'p');
-p.innerHTML = `<em> - ${authorDiv.innerText}</em>`;
-authorDiv.parentElement.replaceWidth(p)
-}
+    const authorDiv = block.querySelector(':scope > div > div');
+    if(authorDiv){
+    const p = document.createElement(tagName: 'p');
+    p.innerHTML = `<em> - ${authorDiv.innerText}</em>`;
+    authorDiv.parentElement.replaceWidth(p)
+    }
 }
